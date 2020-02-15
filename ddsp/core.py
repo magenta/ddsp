Update resample() to handle 1-D through 4-D interpolation. * Added test to ensures correct shapes. * Added test to ensure error thrown for wrong dimensionality.
PiperOrigin-RevId: 295252806
Change-Id: I4706273bd40805c7508536ad99f482660462baa0

ddsp/core.py
@@ -87,11 +87,12 @@ def resample(inputs: tf.Tensor,
              n_timesteps: int,
              method: Text = 'linear',
              add_endpoint: bool = True) -> tf.Tensor:
-  """Interpolates a tensor of shape [:, n_frames, :] to [:, n_timesteps, :].
+  """Interpolates a tensor from n_frames to n_timesteps.
 
   Args:
-    inputs: Framewise 2-D or3-D Tensor. Shape [batch_size, n_frames] or shape
-      [batch_size, n_frames, channels].
+    inputs: Framewise 1-D, 2-D, 3-D, or 4-D Tensor. Shape [n_frames],
+      [batch_size, n_frames], [batch_size, n_frames, channels], or
+      [batch_size, n_frames, n_freq, channels].
     n_timesteps: Time resolution of the output signal.
     method: Type of resampling, must be in ['linear', 'cubic', 'window']. Linear
       and cubic ar typical bilinear, bicubic interpolation. Window uses
@@ -102,34 +103,52 @@ def resample(inputs: tf.Tensor,
       the last timestep as the endpoint, producing (n_frames - 1) segments with
       each having a length of n_timesteps / (n_frames - 1).
   Returns:
-    Interpolated 2-D or 3-D Tensor. Shape [batch_size, n_timesteps] or shape
-      [batch_size, n_timesteps, channels].
+    Interpolated 1-D, 2-D, 3-D, or 4-D Tensor. Shape [n_timesteps],
+      [batch_size, n_timesteps], [batch_size, n_timesteps, channels], or
+      [batch_size, n_timesteps, n_freqs, channels].
 
   Raises:
+    ValueError: If method is 'window' and input is 4-D.
     ValueError: If method is not one of 'linear', 'cubic', or 'window'.
   """
   inputs = tf_float32(inputs)
-  methods = {'linear': 0, 'cubic': 2}
-
+  is_1d = len(inputs.shape) == 1
   is_2d = len(inputs.shape) == 2
-  inputs = inputs[:, :, tf.newaxis] if is_2d else inputs
+  is_4d = len(inputs.shape) == 4
 
-  if method in methods:
-    outputs = inputs[:, :, tf.newaxis, :]
+  # Ensure inputs are at least 3d.
+  if is_1d:
+    inputs = inputs[tf.newaxis, :, tf.newaxis]
+  elif is_2d:
+    inputs = inputs[:, :, tf.newaxis]
+
+  def _image_resize(method):
+    """Closure around tf.image.resize."""
+    # Image resize needs 4-D input. Add/remove extra axis if not 4-D.
+    outputs = inputs[:, :, tf.newaxis, :] if not is_4d else inputs
     outputs = tf.compat.v1.image.resize(outputs,
-                                        [n_timesteps, 1],
-                                        method=methods[method],
+                                        [n_timesteps, outputs.shape[2]],
+                                        method=method,
                                         align_corners=not add_endpoint)
-    outputs = outputs[:, :, 0, :]
+    return outputs[:, :, 0, :] if not is_4d else outputs
 
+  # Perform resampling.
+  if method == 'linear':
+    outputs = _image_resize(tf.compat.v1.image.ResizeMethod.BILINEAR)
+  elif method == 'cubic':
+    outputs = _image_resize(tf.compat.v1.image.ResizeMethod.BICUBIC)
   elif method == 'window':
     outputs = upsample_with_windows(inputs, n_timesteps, add_endpoint)
-
   else:
     raise ValueError('Method ({}) is invalid. Must be one of {}.'.format(
-        method, list(methods.keys()) + ['window']))
+        method, "['linear', 'cubic', 'window']"))
 
-  outputs = outputs[:, :, 0] if is_2d else outputs
+  # Return outputs to the same dimensionality of the inputs.
+  if is_1d:
+    outputs = outputs[0, :, 0]
+  elif is_2d:
+    outputs = outputs[:, :, 0]
+
   return outputs
 
 
@@ -151,11 +170,17 @@ def upsample_with_windows(inputs: tf.Tensor,
     Upsampled 3-D tensor. Shape [batch_size, n_timesteps, n_channels].
 
   Raises:
+    ValueError: If input does not have 3 dimensions.
     ValueError: If attempting to use function for downsampling.
     ValueError: If n_timesteps is not divisible by n_frames (if add_endpoint is
       true) or n_frames - 1 (if add_endpoint is false).
   """
   inputs = tf_float32(inputs)
+
+  if len(inputs.shape) != 3:
+    raise ValueError('Upsample_with_windows() only supports 3 dimensions, '
+                     'not {}.'.format(inputs.shape))
+
   # Mimic behavior of tf.image.resize.
   # For forward (not endpointed), hold value for last interval.
   if add_endpoint: