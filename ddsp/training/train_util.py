93139bb0e8696d2a2f442893384374a2dc56aee5
Weronika Ormaniec <werror@google.com>
2020-08-25 19:01:06 +0000

    Add small strategy update

--- ddsp/training/train_util.py
+++ ddsp/training/train_util.py
@@ -67,7 +67,9 @@ def get_strategy(tpu='', cluster_config=''):
     resolver = tf.distribute.cluster_resolver.SimpleClusterResolver(
         cluster_spec=cluster_spec,
         task_type=cluster_config['task']['type'],
-        task_id=cluster_config['task']['index'])
+        task_id=cluster_config['task']['index'],
+        num_accelerators={'GPU': len(tf.config.list_physical_devices('GPU'))},
+        rpc_layer='grpc')
     strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy(
         cluster_resolver=resolver)
   else:
