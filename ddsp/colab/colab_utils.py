6bb4dcaa6014d389dd740e6b7adad0a2c2f1d901
Jesse Engel <jesseengel@google.com>
2021-08-27 17:19:10 -0700

    Default upload() to mix down to mono for tone transfer. Fixes #323. Fixes #247.
    PiperOrigin-RevId: 393458231

--- ddsp/colab/colab_utils.py
+++ ddsp/colab/colab_utils.py
@@ -139,7 +139,8 @@ def record(seconds=3, sample_rate=DEFAULT_SAMPLE_RATE, normalize_db=0.1):
 
 def audio_bytes_to_np(wav_data,
                       sample_rate=DEFAULT_SAMPLE_RATE,
-                      normalize_db=0.1):
+                      normalize_db=0.1,
+                      mono=True):
   """Convert audio file data (in bytes) into a numpy array using Pydub.
 
   Args:
@@ -147,12 +148,15 @@ def audio_bytes_to_np(wav_data,
     sample_rate: Resample recorded audio to this sample rate.
     normalize_db: Normalize the audio to this many decibels. Set to None to skip
       normalization step.
+    mono: Force stereo signals to single channel. If false, output can one or
+      two channels depending on the source signal.
 
   Returns:
-    An array of the recorded audio at sample_rate.
+    An array of the recorded audio at sample_rate, shape [channels, time].
   """
   return note_seq.audio_io.wav_data_to_samples_pydub(
-      wav_data=wav_data, sample_rate=sample_rate, normalize_db=normalize_db)
+      wav_data=wav_data, sample_rate=sample_rate, normalize_db=normalize_db,
+      num_channels=1 if mono else None)
 
 
 def upload(sample_rate=DEFAULT_SAMPLE_RATE, normalize_db=None):
