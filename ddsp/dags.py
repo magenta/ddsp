3cceb0491518e9c2a90d4fc3dede1ac5c3662bec
Yilei Yang <yileiyang@google.com>
2022-04-14 14:35:58 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 441857435

--- ddsp/dags.py
+++ ddsp/dags.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Library of functions and layers of Directed Acyclical Graphs.
 
 DAGLayer exists as an alternative to manually specifying the forward pass in
--- ddsp/processors.py
+++ ddsp/processors.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Library of base Processor and ProcessorGroup.
 
 ProcessorGroup exists as an alternative to manually specifying the forward
