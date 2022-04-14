65a0a63554085b3cceca7e863f770222751ca3db
Yilei Yang <yileiyang@google.com>
2022-04-14 14:35:35 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 441857322

--- ddsp/training/models/autoencoder.py
+++ ddsp/training/models/autoencoder.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Model that encodes audio features and decodes with a ddsp processor group."""
 
 import ddsp
